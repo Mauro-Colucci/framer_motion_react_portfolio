6e7ccf4a50ee600de3652b619250fa506e309961
Mauro Colucci <colucci.mauro@gmail.com>
2022-12-22 23:59:59 -0300

    before content in heading title

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -9,6 +9,7 @@ const Landing = ({ setSelectedPage }) => {
       id="home"
       className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
     >
+      {/* IMAGE SECTION */}
       <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
         {isAboveMediumScreens ? (
           <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]">
@@ -25,14 +26,32 @@ const Landing = ({ setSelectedPage }) => {
             className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
           />
         )}
-
-        <div className="z-30 basis-2/5 mt-12 md:mt-32">
-          <m.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.5 }}
-          ></m.div>
-        </div>
+      </div>
+      {/* MAIN TEXT */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* HEADINGS */}
+        <m.div
+          initial="hidden"
+          whileInView="visible"
+          /*  will run onnce while in view, needs to be 50% in the div */
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl font-playfair z-10 text-center md:text-start">
+            Jane {""}
+            <span className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush before:absolute before:-left-[35px] before:-top-[125px] before:z-[-1]">
+              Esper
+            </span>
+          </p>
+          <p className="mt-10 mb-7 text-sm text-center md:text-start">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            laudantium maiores voluptatem illo veritatis nam non a ab culpa in?
+          </p>
+        </m.div>
       </div>
     </section>
   );
