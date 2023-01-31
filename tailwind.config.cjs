cf31bbcffbfcc469bfb3ecaf6a32c48fedf2ce2e
Mauro Colucci <colucci.mauro@gmail.com>
2023-01-31 02:19:00 -0300

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,10 +25,10 @@ module.exports = {
         opensans: ["Open Sans", "sans-serif"],
       },
       content: {
-        brush: "url('assets/brush.png')",
-        person1: "url('assets/person-1.png')",
-        person2: "url('assets/person-2.png')",
-        person3: "url('assets/person-3.png')",
+        brush: "url('./src/assets/brush.png')",
+        person1: "url('./src/assets/person-1.png')",
+        person2: "url('./src/assets/person-2.png')",
+        person3: "url('./src/assets/person-3.png')",
       },
     },
     screens: {
