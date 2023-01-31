e95c012ba3fcd46ff7770214268e693dbc87abca
Mauro Colucci <colucci.mauro@gmail.com>
2023-01-31 01:23:14 -0300

    assets for testimonials added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -26,9 +26,9 @@ module.exports = {
       },
       content: {
         brush: "url('./assets/brush.png')",
-        person1: "url('./assets/person-1.png')",
-        person2: "url('./assets/person-2.png')",
-        person3: "url('./assets/person-3.png')",
+        person1: "url('../assets/person-1.png')",
+        person2: "url('../assets/person-2.png')",
+        person3: "url('../assets/person-3.png')",
       },
     },
     screens: {
